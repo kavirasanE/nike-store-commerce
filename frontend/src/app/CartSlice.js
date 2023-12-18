b939d336442b0124b24b854bea1642d915d9e28f
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-12-18 21:55:30 +0530

    redux setup

---
+++ frontend/src/app/CartSlice.js
@@ -0,0 +1,21 @@
+import {createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartState: false,
+ };
+
+const CartSlice =createSlice({
+     initialState,
+     name:'cart',
+     reducers:{
+        setOpenCart :( state,action) => {
+            state.cartState =action.payload.cartState;
+        },
+        setCloseCart: (state,action) => {
+          state.cartState =action.payload.cartState;
+        }
+     }
+});
+export const { setCloseCart,setOpenCart } = CartSlice.actions;
+
+export default CartSlice.reducer;
